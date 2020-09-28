a159b4c2de2cbc811891b278e97bba33c42ff02b
nathasyae <elioranathasya@gmail.com>
2020-09-28 23:28:48 +0700

    add main code, tests, and apk

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivity.kt
@@ -5,19 +5,25 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val input_name:EditText by lazy { findViewById<EditText>(R.id.inputName) }
+    val display_text:TextView by lazy {findViewById<TextView>(R.id.display)}
+    val submit_button:Button by lazy {findViewById<Button>(R.id.submitButton)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val input_name = findViewById<EditText>(R.id.inputName)
-        val display_text = findViewById<TextView>(R.id.display)
-        val submit_button = findViewById<Button>(R.id.submitButton)
-
         submit_button.setOnClickListener{
-            display_text.text = "Hello, " + input_name.text + "!"
+            if(validateInputName(input_name.text.toString())){
+                display_text.text = "Hello, " + input_name.text.toString() + "!"
+            }
         }
     }
+
+    fun validateInputName(input_name: String): Boolean {
+        return input_name.matches("^[a-zA-Z]*$".toRegex()) and (input_name.length>0)
+    }
+
 }
--- app/src/test/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivityTest.kt
+++ app/src/test/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivityTest.kt
@@ -1,5 +1,20 @@
 package id.ac.ui.cs.mobileprogramming.nathasyaeliora.myapplication
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Test
 
-class MainActivityTest
+class MainActivityTest{
+    val mainAct = MainActivity()
+
+    @Test
+    fun testInputNameEmpty(){
+        assertEquals(mainAct.validateInputName(""),false)
+    }
+
+    @Test
+    fun testInputNameValid() {
+        assertEquals(mainAct.validateInputName("Squishy"),true)
+    }
+
+}
