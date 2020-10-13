a58593425848f69d79a3829428109eda45528b4e
nathasyae <elioranathasya@gmail.com>
2020-10-13 09:15:12 +0700

    tambahan resources provider - topik sharing 5

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package id.ac.ui.cs.mobileprogramming.nathasyaeliora.myapplication
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -13,6 +15,9 @@ import androidx.navigation.ui.NavigationUI
 class MainActivity : AppCompatActivity() {
     private var navController: NavController? = null
     private var sharedViewModel: SharedViewModel? = null
+    val themeSwitch by lazy { findViewById<Switch>(R.id.themeSwitch) }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController!!)
         sharedViewModel = ViewModelProvider(this).get<SharedViewModel>(SharedViewModel::class.java)
+
+        themeSwitch.setOnCheckedChangeListener{_,checkedId -> when (checkedId) {
+            true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
