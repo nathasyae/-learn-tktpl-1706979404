825e324a32717670614848464b090f0fcf772d8b
nathasyae <elioranathasya@gmail.com>
2020-11-30 16:56:45 +0700

    add wifi scanner functionality

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nathasyaeliora/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.IntentFilter
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,45 +16,71 @@ import kotlinx.android.synthetic.main.row.*
 
 class MainActivity : AppCompatActivity() {
 
-    val listView = findViewById<ListView>(R.id.list_view)
-    val scanBtn = findViewById<Button>(R.id.scan_button)
-    val size: Int = 0
-    val adapter: Adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList)
+    lateinit var adapter: ArrayAdapter<String>
     lateinit var wifiManager: WifiManager
     lateinit var results: List<ScanResult>
-    lateinit var arrayList: ArrayList<String>
+    var arrayStringResult:ArrayList<String> = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("ngetest", "oncreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        scan_button.setOnClickListener{
+            startScanning()
+        }
+
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
         if (!wifiManager.isWifiEnabled) {
             Toast.makeText(this, "Wifi is disabled, turn it on", Toast.LENGTH_LONG).show()
-            wifiManager.isWifiEnabled = true
+            wifiManager.setWifiEnabled(true)
         }
 
-        listView.setAdapter(adapter)
-        startScanning()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayStringResult)
+        list_view.adapter = adapter
+
+
     }
 
-    fun startScanning(){
-        arrayList.clear()
-        registerReceiver(wifiReceiver, IntentFilter(wifiManager.SCAN_RESULT_AVAILABLE_ACTION);
-    }
+    private fun startScanning(){
+        Log.i("ngetest", "masuk startscanning")
+        val wifiScanReceiver = object : BroadcastReceiver() {
 
-    var wifiReceiver:BroadcastReceiver = object:BroadcastReceiver() {
-       override fun onReceive(context:Context, intent:Intent) {
-            results = wifiManager.getScanResults()
-            unregisterReceiver(this)
-
-            for (scanResult in results)
-            {
-                arrayList.add(scanResult.SSID)
-                adapter.notifyDataSetChanged()
+            override fun onReceive(context: Context, intent: Intent) {
+                val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+                if (success) {
+                    scanSuccess()
+                } else {
+                    scanFailure()
+                }
             }
         }
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        applicationContext.registerReceiver(wifiScanReceiver, intentFilter)
+
+        val success = wifiManager.startScan()
+        if (!success) {
+            // scan failure handling
+            scanFailure()
+        }
+    }
+
+    private fun scanSuccess() {
+        val results = wifiManager.scanResults
+        results.forEach {
+            Log.i("ssid", it.toString())
+            this.arrayStringResult.add(it.SSID)
+            adapter.notifyDataSetChanged()
+        }
+        Log.i("ngetest", arrayStringResult.toString())
+    }
+
+    private fun scanFailure() {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        val results = wifiManager.scanResults
     }
 }
-
