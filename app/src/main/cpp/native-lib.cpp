5f604c5a80a8d44858da3eabc42d801aa48c7585
nathasyae <elioranathasya@gmail.com>
2020-12-07 16:57:37 +0700

    change local var, parameter in cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,10 +6,8 @@
 
 extern "C"
 JNIEXPORT jint JNICALL
-Java_id_ac_ui_cs_mobileprogramming_nathasyaeliora_myapplication_calculateTotalPrice(
-        JNIEnv* pEnv,
-        jobject pThis,
-        jint qt) {
-    jlong b = 55000;
+Java_id_ac_ui_cs_mobileprogramming_nathasyaeliora_myapplication_MainActivity_calculteTotalPrice(
+        JNIEnv *env, jobject thiz, jint qt) {
+    jlong b = 55;
     return qt * b;
 }
